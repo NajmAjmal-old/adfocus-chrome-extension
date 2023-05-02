99cd9bd34db9c038095109d5121fd63ec08ae4cf
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-05-02 06:56:17 -0700

    Update content.js

--- content.js
+++ content.js
@@ -1,36 +1,26 @@
-const code = `
-  <script type="text/javascript">
-    var adfly_id = 24692891;
-    var adfly_advert = 'int';
-    var adfly_protocol = 'http';
-    var adfly_domain = 'ads.doc.sintco.cloudns.nz';
-    var exclude_domains = ['example.com'];
-    var frequency_cap = '5';
-    var frequency_delay = '5';
-    var init_delay = '3';
-    var popunder = true;
-  </script>
-  <script src="https://cdn.adf.ly/js/link-converter.js"></script>
-  <script src="https://cdn.adf.ly/js/entry.js"></script>
-`;
+const adFocUsBanner = '<a href="//adfoc.us/?refid=585030" target="_blank"><img src="///images/banners/userbar-1.png" border="0" alt="AdFoc.us Banner"></a>';
 
-const banner = `
-  <!-- Start of adf.ly banner code -->
-  <div style="position:relative;width: 728px; height:90px; text-align: center; font-family: verdana; font-size: 10px;">
-    <div style="position:absolute; top:0; left:0;">
-      <a href="https://join-adf.ly/24692891">
-        <img border="0" src="https://cdn.adf.ly/images/banners/adfly.728x90.2.gif" width="728" height="90" title="AdF.ly - shorten links and earn money!" />
-      </a>
-    </div>
-    <div style="height:90px;"></div>
-  </div>
-  <!-- End of adf.ly banner code -->
-`;
+const adFocUsScript = '<script type="text/javascript">\n\tvar id_user = 585030;\n\tvar domains_exclude = [\'m.com\'];\n</script>\n<script type="text/javascript" src="http://adfoc.us/js/fullpage/script.js"></script>';
 
-const body = document.body || document.getElementsByTagName("body")[0];
-body.insertAdjacentHTML("afterbegin", banner);
-console.log("Adf.ly banner inserted into the body element at the top-middle!");
+// Remove existing banners
+const banners = document.getElementsByTagName('iframe');
+for (let i = 0; i < banners.length; i++) {
+  banners[i].remove();
+}
 
-const head = document.head || document.getElementsByTagName("head")[0];
-head.insertAdjacentHTML("beforeend", code);
-console.log("Adf.ly script inserted into the head element!");
+// Add AdFoc.us banner at the top of the body
+const body = document.getElementsByTagName('body')[0];
+const bannerDiv = document.createElement('div');
+bannerDiv.style.position = 'fixed';
+bannerDiv.style.top = 0;
+bannerDiv.style.left = 0;
+bannerDiv.style.width = '100%';
+bannerDiv.style.zIndex = '999999';
+bannerDiv.innerHTML = adFocUsBanner;
+body.insertBefore(bannerDiv, body.firstChild);
+
+// Add AdFoc.us script to the head
+const head = document.head || document.getElementsByTagName('head')[0];
+head.insertAdjacentHTML('beforeend', adFocUsScript);
+
+console.log('AdFoc.us extension loaded.');
